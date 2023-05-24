a780e4f6c329b5b07dffc882563ca876201ff319
philipheferreira <philipheferreira@gmail.com>
2023-05-24 16:07:49 -0300

    update estrutura padrao

--- EditorHTML/Program.cs
+++ EditorHTML/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EditorHtml { 
+    class Program {  
+        static void Main(string[] args) {
+            Menu.Show();
+        }
+    }
+}
